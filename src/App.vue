5969d946c8a9cb54a3334202ff1a8e7b7dfe3c5b
Andrik0711 <mord.71gamer@gmail.com>
2024-05-29 20:26:57 -0600

    Home Page

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import TheHome from './components/TheHome.vue'
+</script>
+
+<template>
+    <header>
+        <TheHeader />
+    </header>
+
+    <main>
+        <TheHome />
+        
+    </main>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="HeaderButtons" class="flex flex-wrap justify-center animate-fade-in">
+        <button
+            class="hvr-grow-rotate m-2 bg-green-400 text-black p-2 rounded transition duration-300 ease-in-out hover:bg-blue-400"
+        >
+            Home
+        </button>
+        <button
+            class="hvr-grow-rotate m-2 bg-green-400 text-black p-2 rounded transition duration-300 ease-in-out hover:bg-blue-400"
+        >
+            About
+        </button>
+        <button
+            class="hvr-grow-rotate m-2 bg-green-400 text-black p-2 rounded transition duration-300 ease-in-out hover:bg-blue-400"
+        >
+            Projects
+        </button>
+        <button
+            class="hvr-grow-rotate m-2 bg-green-400 text-black p-2 rounded transition duration-300 ease-in-out hover:bg-blue-400"
+        >
+            Contact
+        </button>
+        <button
+            class="hvr-grow-rotate m-2 bg-green-400 text-black p-2 rounded transition duration-300 ease-in-out hover:bg-blue-400"
+        >
+            Experience
+        </button>
+    </div>
+</template>
+
