f112d50c83da1be74aa67db1f4cf23e2c2c1c2f0
Andrik0711 <mord.71gamer@gmail.com>
2024-06-05 15:55:07 -0600

    School certificates, links

--- src/components/TheSchool.vue
+++ src/components/TheSchool.vue
@@ -1,11 +1,15 @@
 <template>
     <section id="TheSchool">
         <h2 id="demoFont" class="animate-blurred-fade-in text-center">Education</h2>
-        <div class="schools animate-blurred-fade-in ">
+        <div class="schools animate-blurred-fade-in">
             <div class="school" v-for="school in schools" :key="school.id">
                 <h3 class="text-center">{{ school.title }}</h3>
-                <p class=" text-justify" >{{ school.description }}</p>
-                <p>{{ school.date }}</p>
+                <p class="text-justify">{{ school.description }}</p>
+                <p>{{ school.date }}
+                    <a v-if="school.img" :href="school.img" target="_blank" class="text-blue-500 hover:underline"
+                        >View Certificate</a
+                    >
+                </p>
             </div>
         </div>
     </section>
@@ -21,19 +25,58 @@ export default {
                     id: 1,
                     title: 'Information Technology Engineering',
                     description: 'POLYTECHNIC UNIVERSITY OF VICTORIA.',
-                    date: 'April, 2024'
+                    date: 'April 18, 2024.',
+                    img: ''
                 },
                 {
                     id: 2,
                     title: 'Technician in Programming',
-                    description: 'INDUSTRIAL AND SERVICES TECHNOLOGICAL BACCALAUREATE CENTER NO. 119',
-                    date: 'August, 2020'
+                    description:
+                        'INDUSTRIAL AND SERVICES TECHNOLOGICAL BACCALAUREATE CENTER NO. 119',
+                    date: 'August 14, 2020.',
+                    img: ''
                 },
                 {
                     id: 3,
-                    title: 'Web Application Development Course',
-                    description: 'Polytechnic University of Victoria.',
-                    date: '2023'
+                    title: 'Introduction to Cybersecurity',
+                    description: 'CISCO NETWORKING ACADEMY.',
+                    date: 'July 17, 2022.',
+                    img: 'certificates/I2CS__1_.webp'
+                },
+                {
+                    id: 4,
+                    title: 'CCNA: Introduction to Networks',
+                    description: 'CISCO NETWORKING ACADEMY.',
+                    date: 'December 06, 2021.',
+                    img: 'certificates/CCNAITN__1_.webp'
+                },
+                {
+                    id: 5,
+                    title: 'CCNA: Switching, Routing, and Wireless Essentials',
+                    description: 'CISCO NETWORKING ACADEMY.',
+                    date: 'March 17, 2022.',
+                    img: 'certificates/CCNASRWE__1_.webp'
+                },
+                {
+                    id: 6,
+                    title: 'CCNA: Enterprise Networking, Security, and Automation',
+                    description: 'CISCO NETWORKING ACADEMY.',
+                    date: 'February, 2023.',
+                    img: 'certificates/CCNAENSA__1_.webp'
+                },
+                {
+                    id: 7,
+                    title: 'Constancia BUAP',
+                    description: 'MERITORIUS AUTONOMOUS UNIVERSITY OF PUEBLA.',
+                    date: 'November 20, 2023.',
+                    img: 'certificates/CONACIC2023_ID_23.webp'
+                },
+                {
+                    id: 7,
+                    title: 'Google User Experience (UX) Design Professional Certificate',
+                    description: 'COURSERA.',
+                    date: 'October 13, 2023.',
+                    img: 'certificates/Coursera-4262QND299G6.webp'
                 }
             ]
         }
